f3624db3dd5fbc5c3c97e18424454a7ca159f4c1
Jinwoo Kim <kidjinwoo@me.com>
2023-12-22 22:02:30 +0900

    no message

--- AttributeScopesConversion/ContentView.swift
+++ AttributeScopesConversion/ContentView.swift
@@ -6,12 +6,38 @@
 //
 
 import SwiftUI
+import UIKit
 import Target
 
 struct ContentView: View {
     var body: some View {
         Color.clear
             .onAppear {
+                var container: AttributeContainer = try! .init(
+                    [
+                        .font: UIFont.systemFont(ofSize: 30.0),
+                        .foregroundColor: UIColor.red,
+                    ],
+                    including: \.uiKit
+                )
+                
+                container.swiftUI.foregroundColor = .red
+                container.swiftUI.kern = 30.0
+                
+                /*
+                 (label: Optional("storage"), value: {
+                     NSColor = UIExtendedSRGBColorSpace 1 0 0 1
+                     NSFont = <UICTFont: 0x10270a7d0> font-family: ".SFUI-Regular"; font-weight: normal; font-style: normal; font-size: 30.00pt
+                     SwiftUI.ForegroundColor = red
+                     SwiftUI.Kern = 30.0
+                 })
+                 */
+                Mirror(reflecting: container)
+                    .children
+                    .forEach { child in
+                        print(child)
+                    }
+                
                 var attributedString: AttributedString = .init(stringLiteral: "Hello World!")
                 attributedString.foregroundColor = .red
                 
--- Target/foo.swift
+++ Target/foo.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SwiftUI
 
 public func printForegroundColor(attributedString: AttributedString) {
     print(attributedString.foregroundColor ?? "nil")
